0ea69d1fe9def7f86b88793c57a0dcb4f7acc579
dno18 <128874058+dno18@users.noreply.github.com>
2026-04-19 11:56:04 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,20 @@
-module.exports = async function handler(req, res) {
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
   const { url } = req.query;
   if (!url) return res.status(400).send('URL required');
+
   try {
     const response = await fetch(decodeURIComponent(url), {
       headers: { 'User-Agent': 'Mozilla/5.0', 'Referer': 'https://www.tiktok.com/' }
     });
+    
     res.setHeader('Content-Type', response.headers.get('content-type'));
     res.setHeader('Access-Control-Allow-Origin', '*');
+    
     const buffer = await response.arrayBuffer();
     res.send(Buffer.from(buffer));
-  } catch (e) { res.status(502).send('Proxy error'); }
+  } catch (e) {
+    res.status(502).send('Proxy error');
+  }
 };
