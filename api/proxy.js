11c0a334e2fe60a087290e7496cc1095cd657d26
dno18 <128874058+dno18@users.noreply.github.com>
2026-04-22 18:16:58 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
     const { url } = req.query;
+    if (!url) return res.status(400).send('No URL');
     try {
         const response = await fetch(url);
         const buffer = await response.arrayBuffer();
         res.setHeader('Content-Type', 'video/mp4');
+        res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
         res.send(Buffer.from(buffer));
     } catch (e) {
-        res.status(500).send('Proxy Error');
+        res.status(500).send('Error');
     }
 }
